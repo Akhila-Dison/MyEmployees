3fef731e6737afd3aa47004368184ec477c02708
akhil <akhil@EM>
2024-07-10 01:55:06 +0530

    Akhila

---
+++ MyEmployees/Pages/Employees/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyEmployees.Pages.Employees
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
