9631d789d2bacaff389f3d272a685556a7e8f0a9
akhil <akhil@EM>
2024-07-11 02:54:19 +0530

    Creation of  Index form and New Empoyee form completed.

--- MyEmployees/Pages/Employees/Index.cshtml.cs
+++ MyEmployees/Pages/Employees/Index.cshtml.cs
@@ -1,12 +1,58 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
 namespace MyEmployees.Pages.Employees
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public List<EmployeeInfo> listEmployees = new List<EmployeeInfo>();
+ 
+
+public void OnGet()
         {
+            try
+            {
+                string connectionString = "Data Source=.\\sqlexpress;Initial Catalog=MyEmployees;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+                using (SqlConnection connection = new SqlConnection(connectionString)) 
+                {
+                    connection.Open ();
+                    String sql = " SELECT * FROM Employee";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                EmployeeInfo employeeInfo = new EmployeeInfo();
+                                employeeInfo.id = "" + reader.GetInt32(0);
+                                employeeInfo.name = reader.GetString(1);
+                                employeeInfo.email = reader.GetString(2);
+                                employeeInfo.phone = reader.GetString(3);
+                                employeeInfo.address = reader.GetString(4);
+                                employeeInfo.created_at = reader.GetDateTime(5).ToString();
+
+                                listEmployees.Add (employeeInfo);
+                            }
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine ("Exception:" +ex.ToString());
+            }
         }
+    }
+
+    public class  EmployeeInfo
+    {
+        public String id;
+        public String name;
+        public String email;
+        public String phone;
+        public String address;
+        public String created_at;
     }
 }
---
+++ MyEmployees/Pages/Employees/frmCreateEmployee.cshtml.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace MyEmployees.Pages.Employees
+{
+    public class frmCreateEmployeeModel : PageModel
+    {
+        public EmployeeInfo EmployeeInfo = new EmployeeInfo();
+        public string errorMessage = "";
+        public string successMessage = "";
+        public void OnGet()
+        {
+        }
+        public void OnPost() 
+        {
+            EmployeeInfo.name = Request.Form["name"];
+            EmployeeInfo.email = Request.Form["email"];
+            EmployeeInfo.phone = Request.Form["phone"];
+            EmployeeInfo.address = Request.Form["address"];
+
+            if (EmployeeInfo.name.Length == 0 || EmployeeInfo.email.Length == 0 || EmployeeInfo.phone.Length == 0 || EmployeeInfo.address.Length == 0)
+            {
+                errorMessage = "All the fields are required";
+                return;
+            }
+
+            // Save to database.
+            try
+            {
+                string connectionString = "Data Source=.\\sqlexpress;Initial Catalog=MyEmployees;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                { 
+                connection.Open();
+                    String sql = "INSERT INTO Employee (name,email,phone,address)" +
+                                 " VALUES (@name,@email,@phone,@address);";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    { 
+                    command.Parameters.AddWithValue("@name", EmployeeInfo.name);
+                    command.Parameters.AddWithValue("@email", EmployeeInfo.email);
+                    command.Parameters.AddWithValue("@phone", EmployeeInfo.phone);
+                    command.Parameters.AddWithValue("@address", EmployeeInfo.address);
+
+                    command.ExecuteNonQuery();
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+                return;
+            }
+
+            EmployeeInfo.name = "";
+            EmployeeInfo.name = "";
+            EmployeeInfo.name = "";
+            EmployeeInfo.name = "";
+            successMessage = "New employee saved successfully";
+
+            Response.Redirect("/Employees/Index");
+
+
+        }
+    }
+}
